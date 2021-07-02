8d4292d59e3e65a37498d3f36b13f0f9676aa8f9
Harjot Singh <asingh.harjot@gmail.com>
2021-07-02 13:51:09 +0530

    animation repeats on each refresh

--- lib/main.dart
+++ lib/main.dart
@@ -27,9 +27,12 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   final RefreshController _refreshController =
       RefreshController(initialRefresh: false);
+
+  late AnimationController _controller;
 
   void _onRefresh() async {
     print("on refresh");
@@ -37,7 +40,22 @@ class _HomePageState extends State<HomePage> {
     await Future.delayed(Duration(milliseconds: 1000));
     // if failed,use refreshFailed()
     _refreshController.refreshCompleted();
+    _controller.forward(from: 0);
     setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    _controller.forward();
+    // _controller.addListener(() {
+    //   // print(_controller.value);
+    // });
+    // _controller.addStatusListener((status) {
+    //   // print("controller status= $status");
+    // });
   }
 
   @override
@@ -66,9 +84,18 @@ class _HomePageState extends State<HomePage> {
           itemBuilder: (context, index) {
             int r = Random().nextInt(10);
             if (index % 2 == 0)
-              return LeftTile(
-                  data[r]["imagePath"] ?? "", data[r]["text"] ?? "");
-            return RightTile(data[r]["imagePath"] ?? "", data[r]["text"] ?? "");
+              return SlideTransition(
+                position: Tween<Offset>(begin: Offset(-1, 0), end: Offset(0, 0))
+                    .animate(_controller),
+                child:
+                    LeftTile(data[r]["imagePath"] ?? "", data[r]["text"] ?? ""),
+              );
+            return SlideTransition(
+              position: Tween<Offset>(begin: Offset(1, 0), end: Offset(0, 0))
+                  .animate(_controller),
+              child:
+                  RightTile(data[r]["imagePath"] ?? "", data[r]["text"] ?? ""),
+            );
           },
         ),
       ),
@@ -94,33 +121,25 @@ class _LeftTileState extends State<LeftTile> {
   Widget build(BuildContext context) {
     int r = Random().nextInt(10);
     print("build left tile $r");
-    return TweenAnimationBuilder(
-      tween: Tween<Offset>(begin: Offset(startPos, 0), end: Offset(endPos, 0)),
-      curve: curve,
-      duration: Duration(seconds: 1),
-      builder: (context, Offset value, child) {
-        return FractionalTranslation(translation: value, child: child);
-      },
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Container(
-          width: MediaQuery.of(context).size.width - 100,
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-          margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage(
-                widget.imagePath,
-              ),
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        width: MediaQuery.of(context).size.width - 100,
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(
+              widget.imagePath,
             ),
           ),
-          child: Center(
-            child: Text(
-              "${widget.text}?",
-              style: TextStyle(color: Colors.white),
-            ),
+        ),
+        child: Center(
+          child: Text(
+            "${widget.text}?",
+            style: TextStyle(color: Colors.white),
           ),
         ),
       ),
@@ -146,33 +165,25 @@ class _RightTileState extends State<RightTile> {
   Widget build(BuildContext context) {
     int r = Random().nextInt(10);
     print("build right tile $r");
-    return TweenAnimationBuilder(
-      tween: Tween<Offset>(begin: Offset(startPos, 0), end: Offset(endPos, 0)),
-      duration: Duration(seconds: 1),
-      curve: curve,
-      builder: (context, Offset value, child) {
-        return FractionalTranslation(translation: value, child: child);
-      },
-      child: Align(
-        alignment: Alignment.centerRight,
-        child: Container(
-          width: MediaQuery.of(context).size.width - 100,
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-          margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage(
-                widget.imagePath,
-              ),
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Container(
+        width: MediaQuery.of(context).size.width - 100,
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(
+              widget.imagePath,
             ),
           ),
-          child: Center(
-            child: Text(
-              "${widget.text}?",
-              style: TextStyle(color: Colors.white),
-            ),
+        ),
+        child: Center(
+          child: Text(
+            "${widget.text}?",
+            style: TextStyle(color: Colors.white),
           ),
         ),
       ),
